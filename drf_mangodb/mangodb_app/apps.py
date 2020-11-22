69dd8af478be5fa6aba3d4a315702a387fa0df1f
= <=>
2020-11-22 15:05:18 +0800

    local library

---
+++ drf_mangodb/mangodb_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MangodbAppConfig(AppConfig):
+    name = 'mangodb_app'
---
+++ localLibrary/catalog/urls.py
@@ -0,0 +1,14 @@
+# Use include() to add paths from the catalog application 
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include
+from django.views.generic import RedirectView
+
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+]
