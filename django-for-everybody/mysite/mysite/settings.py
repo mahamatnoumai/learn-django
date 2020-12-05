5aa35e7802ce1407f2923a2559fb5be72bf553b4
= <=>
2020-12-05 11:17:25 +0800

    dj4e

--- django-for-everybody/mysite/mysite/settings.py
+++ django-for-everybody/mysite/mysite/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'jgr=)0=9-0rp@n9v5rwwq7#k(otksyz6ocy*x53t3_@n^*3ah2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- django-for-everybody/mysite/polls/views.py
+++ django-for-everybody/mysite/polls/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. 22dabd20 is the polls index.")
